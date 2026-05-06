818c91f22d6c41e8b5fb80bda4c7e760f3d9e3f5
Morten Aage Hansen <morten.aage.hansen@gmail.com>
2026-05-06 20:57:00 +0200

    upd

--- test/test_example_lib.cpp
+++ test/test_example_lib.cpp
@@ -8,7 +8,14 @@ TEST(ExampleLibTest, Add) {
     EXPECT_EQ(lib.add(0, 0), 0);
 }
 
-TEST(ExampleLibTest1, Subtract) {
+TEST(ExampleLibTest1, Add) {
+    ExampleLib lib;
+    EXPECT_EQ(lib.add(3, 4), 7);
+    EXPECT_EQ(lib.add(-1, 1), 0);
+    EXPECT_EQ(lib.add(0, 0), 0);
+}
+
+TEST(ExampleLibTest2, Subtract) {
     ExampleLib lib;
     EXPECT_EQ(lib.subtract(10, 3), 7);
     EXPECT_EQ(lib.subtract(0, 5), -5);
